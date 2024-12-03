6684a4e364f3798555e3f8e3593bc0a9a4399bde
David <harlems-finest@hotmail.de>
2024-12-03 13:55:31 +0100

    dateiErstellen() methode ist geschrieben ... muss aber och etwas verberssert in der ausgabe werden

--- src/NextLineNeat.java
+++ src/NextLineNeat.java
@@ -1,5 +1,7 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.*;
 
 public class NextLineNeat {
@@ -7,6 +9,7 @@ public class NextLineNeat {
     static HashSet<String> wordHashSet = new HashSet<>();
     static Map<String, Integer> wordMap = new TreeMap<>();
     static SortedMap<Integer, ArrayList<String>> sortedWordArrList = new TreeMap<>(Comparator.reverseOrder());
+    static ArrayList<String> arrayList = new ArrayList<>();
 
     public static String eingabe() {
         Scanner scan = new Scanner(System.in);
@@ -24,7 +27,7 @@ public class NextLineNeat {
     }
 
     public static void splitten(File datei) {
-        if (datei.exists()){
+        if (datei.exists()) {
             System.out.println("Datei gefunden: " + datei.getAbsolutePath());
         } else {
             System.out.println("Datei nicht gefunden. ");
@@ -73,6 +76,29 @@ public class NextLineNeat {
         System.out.println("Wörter insgesamt: " + wordArrList.size());
     }
 
+    public static void dateiErstellen() {
+        try {
+            if (!wordMap.isEmpty()) {
+                FileWriter fileWriter = new FileWriter("C:/Users/Administrator/Desktop/testerstellt.txt");
+                for (Map.Entry<String, Integer> entry : wordMap.entrySet()) {
+                    int count = entry.getValue();
+//                    arrayList.add(String.valueOf(entry.getValue()));
+                    sortedWordArrList.putIfAbsent(count, new ArrayList<>());
+                    sortedWordArrList.get(count).add(entry.getKey());
+                }
+                for (Map.Entry<Integer, ArrayList<String>> entry : sortedWordArrList.entrySet()) {
+                    String key = String.valueOf(entry.getKey());
+                    String value = String.valueOf(entry.getValue());
+                    fileWriter.write(key + value);
+                }
+                fileWriter.close();
+                System.out.println("Datei erstellt. ");
+            }
+        } catch (IOException e) {
+//            throw new RuntimeException(e);
+        }
+    }
+
     //##################################################################################################################
     public static void main(String[] args) {
 
@@ -82,5 +108,7 @@ public class NextLineNeat {
         einlesenAusgebenWordMap();
         einlesenAusgebenWordHashSet();
         ausgabeWoerterInsgesamt();
+//        TODO mit bufferedReader alle wörter in eine datei persistent machen
+        dateiErstellen();
     }
 }
